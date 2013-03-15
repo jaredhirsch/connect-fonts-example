20a810ea37bda243c285738f8b9eecb72fa8984a
Jared Hirsch <ohai@6a68.net>
2013-03-15 15:25:29 -0700

    added final bits, off to github with ye

---
+++ README.md
@@ -0,0 +1,3 @@
+connect-fonts worked example to go with hacks.mozilla.org blog post.
+
+enjoy.
--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const
 ejs = require('ejs'),
 express = require('express'),
 fs = require('fs'),
+// note! we set the port via env var
 port = process.env['PORT'] || 8765,
 font_middleware = require('connect-fonts'),
 opensans = require('connect-fonts-opensans');
@@ -10,22 +11,22 @@ var app = express.createServer(),
   tpl = fs.readFileSync(__dirname + '/tpl.ejs', 'utf8');
 
 // connect-fonts setup.
-// allow_origin is used to add an 'Access-Control-Allow-Origin' header,
-// required by FF to serve fonts cross-domain
+//
+// allow_origin sets an 'Access-Control-Allow-Origin' header,
+// required by Firefox to serve fonts cross-domain.
 app.use(font_middleware.setup({
   fonts: [opensans],
   allow_origin: 'http://localhost:' + port
 }));
 
-// this endpoint serves up ALL THE THINGS: the default font.
-// have a look at the download size in ur browser.
+// this endpoint serves up the whole default font.
+// have a look at the download size in your browser.
 app.get('/default', function(req, res) {
   var rendered = ejs.render(tpl, {fontLocale: 'default'});
   res.send(rendered);
 });
 
-// here, just the english bits.
-// compare in ur dev tools. much smaller!
+// here, just the english bits. much smaller!
 app.get('/en', function(req, res) {
   var rendered = ejs.render(tpl, {fontLocale: 'en'});
   res.send(rendered);
@@ -42,8 +43,10 @@ app.get('/detect', function(req, res) {
 
 function somehowGetLocale() {
   // in real life, use 'i18n-abide' or 'locale' to get this info.
-  // hard-coding to keep the example simple. /me ducks...
+  // for instance, see https://github.com/mozilla/i18n-abide/blob/648dec4e203fbe93ea99cb7718b5106be9a30c82/lib/i18n.js#L188
+  // hard-coding to keep the example simple.
   return 'en';
 }
 
 app.listen(port, '127.0.0.1');
+console.log('connect-fonts-example now running on 127.0.0.1, port ' + port);
