6081cfc00a79653f8a163cfd8042596a9bad9d84
Jared Hirsch <ohai@6a68.net>
2013-03-15 13:05:39 -0700

    actually send back the substituted template, not the raw tpl...

--- example.js
+++ example.js
@@ -4,33 +4,33 @@ const font_middleware = require('connect-fonts'),
   fs = require('fs');
 
 var app = require('express').createServer(),
-  tpl = fs.readFileSync(__dirname + 'tpl.ejs', 'utf8');
+  tpl = fs.readFileSync(__dirname + '/tpl.ejs', 'utf8');
 
 // connect-fonts setup
 app.use(font_middleware.setup({
   fonts: [opensans],
   allow_origin: 'http://localhost'
-});
+}));
 
 // ALL THE THINGS in the default font.
 // have a look at the download size in ur browser.
 app.get('/default', function(req, res) {
-  ejs.render(tpl, {fontLocale = 'default'});
-  res.send(tpl);
+  var rendered = ejs.render(tpl, {fontLocale: 'default'});
+  res.send(rendered);
 });
 
 // just the english bits kthx. smaller!
 app.get('/en', function(req, res) {
-  ejs.render(tpl, {fontLocale = 'en'});
-  res.send(tpl);
+  var rendered = ejs.render(tpl, {fontLocale: 'en'});
+  res.send(rendered);
 });
 
 // cleverly guess the user's locale based on Accept-Language searching
 app.get('/detect', function(req, res) {
-  var clientLocale;
   // TODO use locale to parse this out.
-  ejs.render(tpl, {fontLocale = clientLocale});
-  res.send(tpl);
+  var clientLocale;
+  var rendered = ejs.render(tpl, {fontLocale: clientLocale});
+  res.send(rendered);
 });
 
 app.listen(process.env['PORT'] || 8765, '127.0.0.1');
