b6291863634bd98181c8b4a1b67813345b313f30
Jared Hirsch <ohai@6a68.net>
2013-03-15 12:56:23 -0700

    wips.

--- example.js
+++ example.js
@@ -1,20 +1,35 @@
-const connectFonts = require('connect-fonts');
+const font_middleware = require('connect-fonts'),
+  opensans = require('connect-fonts-opensans'),
+  ejs = require('ejs'),
+  fs = require('fs');
 
 var app = require('express').createServer(),
-  tpl = '<!doctype html><title>fonty mcgoodtimes at ur srvc.</title><p>eye arr a fonted one.</p>';
+  tpl = fs.readFileSync(__dirname + 'tpl.ejs', 'utf8');
 
-// ALL THE THINGS in the default font
+// connect-fonts setup
+app.use(font_middleware.setup({
+  fonts: [opensans],
+  allow_origin: 'http://localhost'
+});
+
+// ALL THE THINGS in the default font.
+// have a look at the download size in ur browser.
 app.get('/default', function(req, res) {
+  ejs.render(tpl, {fontLocale = 'default'});
   res.send(tpl);
 });
 
-// latinates assembled, nobody mentioned the germanics, they're here too
-app.get('/latin', function(req, res) {
-  res.send(tpl);
-});
-
-// Just the english bits kthx
+// just the english bits kthx. smaller!
 app.get('/en', function(req, res) {
+  ejs.render(tpl, {fontLocale = 'en'});
+  res.send(tpl);
+});
+
+// cleverly guess the user's locale based on Accept-Language searching
+app.get('/detect', function(req, res) {
+  var clientLocale;
+  // TODO use locale to parse this out.
+  ejs.render(tpl, {fontLocale = clientLocale});
   res.send(tpl);
 });
 
