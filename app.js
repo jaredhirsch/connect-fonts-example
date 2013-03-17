cdfd5467f772e4f44f583f9915ab032e7539b3e8
Jared Hirsch <ohai@6a68.net>
2013-03-17 16:09:42 -0700

    example seems to be mutating as I write the article

--- app.js
+++ app.js
@@ -21,27 +21,35 @@ app.use(font_middleware.setup({
 
 // this endpoint serves up the whole default font.
 // have a look at the download size in your browser.
-app.get('/default', function(req, res) {
-  var rendered = ejs.render(tpl, {fontLocale: 'default'});
-  res.send(rendered);
+app.get('/time/full_font', function(req, res) {
+  var output = ejs.render(tpl, {
+    fontLocale: 'default',
+    currentTime: new Date()
+  });
+  res.send(output);
 });
 
 // here, just the english bits. much smaller!
-app.get('/en', function(req, res) {
-  var rendered = ejs.render(tpl, {fontLocale: 'en'});
-  res.send(rendered);
+app.get('/time/en', function(req, res) {
+  var output = ejs.render(tpl, {
+    fontLocale: 'en',
+    currentTime: new Date()
+  });
+  res.send(output);
 });
 
 // this is how you'd do it in real life:
 // guess the user's locale based on parsing the 'Accept-Language' header.
 // Persona uses the i18n-abide library for this. YMMV.
-app.get('/detect', function(req, res) {
-  var clientLocale = somehowGetLocale();
-  var rendered = ejs.render(tpl, {fontLocale: clientLocale});
-  res.send(rendered);
+app.get('/time', function(req, res) {
+  var output = ejs.render(tpl, {
+    fontLocale: detectLocale(),
+    currentTime: new Date()
+  });
+  res.send(output);
 });
 
-function somehowGetLocale() {
+function detectLocale() {
   // in real life, use 'i18n-abide' or 'locale' to get this info.
   // for instance, see https://github.com/mozilla/i18n-abide/blob/648dec4e203fbe93ea99cb7718b5106be9a30c82/lib/i18n.js#L188
   // hard-coding to keep the example simple.
